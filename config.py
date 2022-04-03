8da38f3227c0620dd772a726360e01c6e29b83f1
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2022-04-03 19:52:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN = os.environ.get("TOKEN") # your discord bot's token here!
-PREFIX = "$" # your bot's prefix
+BOT_TOKEN = os.environ.get("BOT_TOKEN") # your discord bot's token here!
+PREFIX = os.environ.get("BOT_PREFIX") # your bot's prefix
