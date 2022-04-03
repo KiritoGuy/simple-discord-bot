e12fba1832a79e404a41e46dfab624a9e40c605c
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2022-04-03 20:08:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN") # your discord bot's token here!
-PREFIX = os.environ.get("BOT_PREFIX") # your bot's prefix
+PREFIX = os.environ.get("BOT_PREFIX") # your bot's prefix.
+ERROR_CHANNEL = os.environ.get("ERROR_CHANNEL") # all the error's caused by command and events, etc will send here.
